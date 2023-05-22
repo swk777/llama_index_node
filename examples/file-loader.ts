db22861d856edabcbf5251bcf092d139e593c7ac
swk777 <swk777@gmail.com>
2023-05-22 17:02:16 +0800

    relocate data

--- examples/file-loader.ts
+++ examples/file-loader.ts
@@ -1,6 +1,8 @@
 import { SimpleDirectoryReader } from '../src/readers/file/SimpleDirectoryReader.js'
 
-const documents = new SimpleDirectoryReader('examples/file-types').loadData()
+const documents = new SimpleDirectoryReader(
+  'examples/data/file-types'
+).loadData()
 documents.then(async docs => {
   console.log(docs)
 })
